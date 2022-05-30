12b438db5c3890b7c52868994556f6961deca470
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-30 11:32:28 +0100

    Add Sidebar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
-  
-return (
-    <div className="app">
-        <div className="app__body"></div>
-    </div>
-);
+    return (
+        <div className="app">
+            <div className="app__body">
+                <Sidebar />
+            </div>
+        </div>
+    );
 };
-
 export default App;
