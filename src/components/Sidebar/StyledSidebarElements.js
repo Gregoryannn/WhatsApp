2af9837ee1ab12ee504e50dbcf0fe90c7902a5ac
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-30 11:45:01 +0100

    Style Container, Header and HeaderIcons in Sidebar

--- src/components/Sidebar/StyledSidebarElements.js
+++ src/components/Sidebar/StyledSidebarElements.js
@@ -1,1 +1,21 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex: 0.35;
+`;
+
+export const Header = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 20px;
+    border-right: 1px solid lightgray;
+`;
+
+export const HeaderIcons = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    min-width: 10vw;
+`; 
