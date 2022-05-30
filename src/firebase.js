5d1d46a8b54b38641943bdbafbc547c844720f49
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-30 11:28:06 +0100

    Add firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCNW_XzB5A7-oSAxokXUWXHJfCDIAfqmu0",
+    authDomain: "whatsapp-clone-28532.firebaseapp.com",
+    projectId: "whatsapp-clone-28532",
+    storageBucket: "whatsapp-clone-28532.appspot.com",
+    messagingSenderId: "68843801645",
+    appId: "1:68843801645:web:9288e6710b5a2fc5d4fca4",
+};
