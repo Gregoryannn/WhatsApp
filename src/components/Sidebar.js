402f45abcdd0375d55712966add14a1b1d6de9fa
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-31 09:35:33 +0100

    Add search in the Sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,7 @@
 import React from "react";
 import '../styles/Sidebar.css'
-
 import { Avatar, IconButton } from "@material-ui/core";
 import { Chat, DonutLarge, MoreVert, SearchOutlined } from "@material-ui/icons";
-
 const Sidebar = () => {
     return (
         <div className="sidebar">
@@ -21,8 +19,15 @@ const Sidebar = () => {
                     </IconButton>
                 </div>
             </div>
+
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Search or start new chat" type="text" />
+                </div>
+            </div>
+
         </div>
     );
 };
-
 export default Sidebar;
