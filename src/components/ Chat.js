66aec8bc9020a33fdb1b48bbab823eae39895dc0
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-06-01 14:41:30 +0100

    Add chat__body and chat__message

--- src/components/ Chat.js
+++ src/components/ Chat.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import '../styles/Chat.css'
-
 import { Avatar, IconButton } from "@material-ui/core";
 import {
     AttachFile,
@@ -9,32 +8,41 @@ import {
     InsertEmoticon,
     Mic,
 } from "@material-ui/icons";
-
 const Chat = () => {
     ;
     return (
-            <div className="chat">
-                <div className="chat__header">
-                    <Avatar src="" alt="avatar" />
-                    <div className="chat__headerInfo">
-                        <h3>Room name</h3>
-                        <p>Lase seen at...</p>
-                    </div>
-                    <div className="chat__headerRight">
-                        <IconButton>
-                            <SearchOutlined />
-                        </IconButton>
-                        <IconButton>
-                            <AttachFile />
-                        </IconButton>
-                        <IconButton>
-                            <MoreVert />
-                        </IconButton>
-                    </div>
+        <div className="chat">
+            <div className="chat__header">
+                <Avatar src="" alt="avatar" />
+                <div className="chat__headerInfo">
+                    <h3>Room name</h3>
+                    <p>Lase seen at...</p>
+                </div>
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
                 </div>
             </div>
-        
-       );
-}
 
-            export default Chat;
+            <div className="chat__body">
+                <p className="chat__message">
+                    <span className="chat__name">Caia</span>
+                    This si a message
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+
+            </div>
+
+        </div>
+    );
+}
+export default Chat;
