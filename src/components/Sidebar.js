f6b1537ad4c14f61e8d361c7949dcf92c85b033d
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-06-01 14:39:28 +0100

    Clean up code

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,38 +1,38 @@
 import React from "react";
-import '../styles/Sidebar.css'
+import "../styles/Sidebar.css";
 import { Avatar, IconButton } from "@material-ui/core";
 import { Chat, DonutLarge, MoreVert, SearchOutlined } from "@material-ui/icons";
-
 import SidebarChat from "./SidebarChat";
 
-const Sidebar = () => {
-    return (
-        <div className="sidebar">
-            <div className="sidebar__header">
-                <Avatar src="" alt="avatar" />
-                <div className="sidebar__headerRight">
-                    <IconButton>
-                        <DonutLarge />
-                    </IconButton>
-                    <IconButton>
-                        <Chat />
-                    </IconButton>
-                    <IconButton>
-                        <MoreVert />
-                    </IconButton>
+    const SideBar = () => {
+        return (
+            <div className="sidebar">
+                <div className="sidebar__header">
+                    <Avatar src="" alt="avatar" />
+                    <div className="sidebar__headerRight">
+                        <IconButton>
+                            <DonutLarge />
+                        </IconButton>
+                        <IconButton>
+                            <Chat />
+                        </IconButton>
+                        <IconButton>
+                            <MoreVert />
+                        </IconButton>
+                    </div>
                 </div>
-            </div>
-            <div className="sidebar__search">
-                <div className="sidebar__searchContainer">
-                    <SearchOutlined />
-                    <input placeholder="Search or start new chat" type="text" />
+                <div className="sidebar__search">
+                    <div className="sidebar__searchContainer">
+                        <SearchOutlined />
+                        <input placeholder="Search or start new chat" type="text" />
+                    </div>
                 </div>
-            </div>
 
-            <div className="sidebar__chats">
-                <SidebarChat />
+                <div className="sidebar__chats">
+                    <SidebarChat />
+                </div>
             </div>
-        </div>
-    );
-};
-export default Sidebar;
+        );
+    };
+
+    export default SideBar; 
