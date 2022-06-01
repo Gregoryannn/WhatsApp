f1c1d22d9cd4c78e29284327d2723e4ca4ee111a
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-06-02 00:27:31 +0100

    Connect with pusher

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { useEffect } from "react";
+import Pusher from "pusher-js";
 import "./App.css";
-import Chat from "./components/Chat";
 import Sidebar from "./components/Sidebar";
+import Chat from "./components/Chat";
+
 
 const App = () => {
+    useEffect(() => {
+        const pusher = new Pusher("163612a7213d95a362bb", {
+            cluster: "mt1",
+        });
+
+        const channel = pusher.subscribe("messages");
+        channel.bind("inserted", function (data) {
+            alert(JSON.stringify(data));
+        });
+    }, []);
+
     return (
         <div className="app">
             <div className="app__body">
