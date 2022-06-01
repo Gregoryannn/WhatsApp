8770b5be628612b027d6074c6049b5d34231abaf
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-06-02 00:35:55 +0100

    Refactor with styled-components

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
-import { useEffect, useState } from "react";
-import Pusher from "pusher-js";
 import "./App.css";
-import Sidebar from "./components/Sidebar";
-import Chat from "./components/Chat";
-import axios from "./axios";
+
+import Home from "./page/Home";
+
 const App = () => {
-    const [messages, setMessages] = useState([]);
-    useEffect(() => {
-        axios.get("/messages/sync").then((res) => {
-            setMessages(res.data);
-        });
-    }, []);
-    useEffect(() => {
-        const pusher = new Pusher("163612a7213d95a362bb", {
-            cluster: "mt1",
-        });
-        const channel = pusher.subscribe("messages");
-        channel.bind("inserted", function (newMessage) {
-            setMessages([...messages, newMessage]);
-        });
-        return () => {
-            channel.unbind_all();
-            channel.unsubscribe();
-        };
-    }, [messages]);
-    console.log(messages);
     return (
         <div className="app">
-            <div className="app__body">
-                <Sidebar />
-                 <Chat messages={messages} />
-            </div>
+
+            <Home />
         </div>
     );
 };
---
+++ src/page/Home.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import styled from 'styled-components';
+import Pusher from "pusher-js";
+import axios from "../axios";
+
+import Sidebar from '../components/Sidebar';
+import Chat from '../components/Chat';
+
+const Home = () => {
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        axios.get("/messages/sync").then((res) => {
+            setMessages(res.data);
+        });
+    }, []);
+
+    useEffect(() => {
+        const pusher = new Pusher("163612a7213d95a362bb", {
+            cluster: "mt1",
+        });
+
+        const channel = pusher.subscribe("messages");
+        channel.bind("inserted", function (newMessage) {
+            setMessages([...messages, newMessage]);
+        });
+
+        return () => {
+            channel.unbind_all();
+            channel.unsubscribe();
+        };
+    }, [messages]);
+
+    return (
+        <Main>
+            <Sidebar />
+            <Chat />
+        </Main>
+    )
+}
+
+const Main = styled.main`
+    display: flex;
+    background-color: #ededed;
+    margin-top: -50px;
+    height: 90vh;
+    width: 90vw;
+    box-shadow: -1px 4px 20px -6px rgba(0, 0, 0, 0.75);
+`;
+
+export default Home;
