76c840fcad03b4af5558fe8e5358fac476c82f02
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-06-02 00:33:31 +0100

    Post messages

--- src/App.js
+++ src/App.js
@@ -4,40 +4,32 @@ import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
 import axios from "./axios";
-
 const App = () => {
     const [messages, setMessages] = useState([]);
-
     useEffect(() => {
         axios.get("/messages/sync").then((res) => {
             setMessages(res.data);
         });
     }, []);
-
     useEffect(() => {
         const pusher = new Pusher("163612a7213d95a362bb", {
             cluster: "mt1",
         });
-
         const channel = pusher.subscribe("messages");
-              channel.bind("inserted", function (newMessage) {
-                setMessages([...messages, newMessage]);
-            });
-    
-
+        channel.bind("inserted", function (newMessage) {
+            setMessages([...messages, newMessage]);
+        });
         return () => {
             channel.unbind_all();
             channel.unsubscribe();
         };
     }, [messages]);
-
     console.log(messages);
-
     return (
         <div className="app">
             <div className="app__body">
                 <Sidebar />
-                <Chat />
+                 <Chat messages={messages} />
             </div>
         </div>
     );
--- src/components/ Chat.js
+++ src/components/ Chat.js
@@ -1,6 +1,8 @@
-import React from 'react'
-import '../styles/Chat.css'
+import React, { useState } from "react";
+import "../styles/Chat.css";
+import axios from "../axios";
 import { Avatar, IconButton } from "@material-ui/core";
+
 import {
     AttachFile,
     MoreVert,
@@ -8,61 +10,76 @@ import {
     InsertEmoticon,
     Mic,
 } from "@material-ui/icons";
-const Chat = () => {
-    ;
-    return (
-        <div className="chat">
-            <div className="chat__header">
-                <Avatar src="" alt="avatar" />
-                <div className="chat__headerInfo">
-                    <h3>Room name</h3>
-                    <p>Lase seen at...</p>
-                </div>
-                <div className="chat__headerRight">
-                    <IconButton>
-                        <SearchOutlined />
-                    </IconButton>
-                    <IconButton>
-                        <AttachFile />
-                    </IconButton>
-                    <IconButton>
-                        <MoreVert />
-                    </IconButton>
-                </div>
-            </div>
-            <div className="chat__body">
-                <p className="chat__message">
-                    <span className="chat__name">Caia</span>
-                    This si a message
-                    <span className="chat__timestamp">
-                        {new Date().toUTCString()}
-                    </span>
-                </p>
-                <p className="chat__message chat__receiver">
-                    <span className="chat__name">Caia</span>
-                    This si a message
-                    <span className="chat__timestamp">
-                        {new Date().toUTCString()}
-                    </span>
-                </p>
-            </div>
 
-            <div className="chat__footer">
-                <InsertEmoticon />
-                <form>
-                    <input
-                        value={input}
-                        onChange={(e) => setInput(e.target.value)}
-                        placeholder="Type a message"
-                        type="text"
-                    />
-                    <button onClick={sendMessage} type="submit">
-                        Send a message
-                    </button>
-                </form>
-                <Mic />
+    const Chat = ({ messages }) => {
+        const [input, setInput] = useState("");
+        const sendMessage = async (e) => {
+            e.preventDefault();
+
+            await axios.post("/messages/new", {
+                name: "CCC",
+                message: input,
+                timestamp: "just now",
+                received: false,
+            });
+
+            setInput("");
+        };
+
+        return (
+            <div className="chat">
+                <div className="chat__header">
+                    <Avatar src="" alt="avatar" />
+                    <div className="chat__headerInfo">
+                        <h3>Room name</h3>
+                        <p>Lase seen at...</p>
+                    </div>
+                    <div className="chat__headerRight">
+                        <IconButton>
+                            <SearchOutlined />
+                        </IconButton>
+                        <IconButton>
+                            <AttachFile />
+                        </IconButton>
+                        <IconButton>
+                            <MoreVert />
+                        </IconButton>
+                    </div>
+                </div>
+
+                <div className="chat__body">
+                    {messages.map((message) => (
+                        <p
+                            className={`chat__message ${!message.received && "chat__receiver"
+                                }`}
+                        >
+                            <span className="chat__name">{message.name}</span>
+                            {message.message}
+                            <span className="chat__timestamp">
+                                {message.timestamp}
+                            </span>
+                        </p>
+                    ))}
+                </div>
+
+                <div className="chat__footer">
+                    <InsertEmoticon />
+                    <form>
+                        <input
+                            value={input}
+                            onChange={(e) => setInput(e.target.value)}
+                            placeholder="Type a message"
+                            type="text"
+                        />
+                        <button onClick={sendMessage} type="submit">
+                            Send a message
+                        </button>
+                    </form>
+                    <Mic />
+                </div>
             </div>
-        </div>
-    );
-}
+        );
+
+};
+
 export default Chat;
