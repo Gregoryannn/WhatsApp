65507d96e7dc405d8b7aabc226f4e2120026d850
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-06-03 00:35:48 +0100

    Add firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyCNW_XzB5A7-oSAxokXUWXHJfCDIAfqmu0",
     authDomain: "whatsapp-clone-28532.firebaseapp.com",
@@ -6,3 +8,11 @@ const firebaseConfig = {
     messagingSenderId: "68843801645",
     appId: "1:68843801645:web:9288e6710b5a2fc5d4fca4",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
