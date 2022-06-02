73dd3022c1b8c43ca65ac84ce2e6ce6f7fe56898
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-06-03 00:21:41 +0100

    Add random avatar in the chat card

--- src/components/Sidebar/ChatCard.js
+++ src/components/Sidebar/ChatCard.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 import {
     ChatCardWrap,
     ChatCardInfo,
@@ -7,9 +8,15 @@ import {
 import { Avatar } from "@material-ui/core";
 
 const ChatCards = () => {
+    const [seed, setSeed] = useState('');
+
+    useEffect(() => {
+        setSeed(Math.floor(Math.random() * 100));
+    }, [])
+
     return (
         <ChatCardWrap>
-            <Avatar src="" alt="avatar" />
+            <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} alt="avatar" />
             <ChatCardInfo>
                 <h2>Room name</h2>
                 <p>This is the last message</p>
